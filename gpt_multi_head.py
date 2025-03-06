f43f5d91a5b0725f1ba82f815d8c868c2da0e244
marimar <luciemarimar.nouboussignintedem@ensae.fr>
2025-03-06 00:18:56 +0000

    TP3 terminé

--- gpt_multi_head.py
+++ gpt_multi_head.py
@@ -11,7 +11,8 @@ learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 32
-
+n_head = 4
+dropout = 0.2
 
 # ------------
 
@@ -63,16 +64,16 @@ def estimate_loss():
 class Head(nn.Module):
     """ one head of self-attention """
 
-    def __init__(self, head_size):
+    def __init__(self,n_embd, head_size):
         super().__init__()
         # YOUR CODE
         # add you key, query and value definitions
-        self.head_size = head_size
-        self.key = nn.Linear(head_size, head_size)
-        self.query = nn.Linear(head_size, head_size)
-        self.value = nn.Linear(head_size, head_size)
-        ###
+        self.key = nn.Linear(n_embd, head_size)
+        self.query = nn.Linear(n_embd, head_size)
+        self.value = nn.Linear(n_embd, head_size)
+        self.dropout = nn.Dropout(dropout)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
 
 
 
@@ -85,13 +86,76 @@ class Head(nn.Module):
         v = self.value(x)  # (B, T, head_size)
         
         # Calcul du produit de Q et K pour obtenir les poids d'attention
-        weights = torch.bmm(q, k.transpose(1, 2))  # (B, T, head_size) @ (B, head_size, T) -> (B, T, T)
-        
-        # Appliquer softmax sur les poids pour obtenir la distribution d'attention
-        weights = torch.nn.functional.softmax(weights, dim=-1)  # (B, T, T)
+        weights = q @ k.transpose(-2, -1) / (k.shape[-1] ** 0.5)  # (B, T, T)
+        weights = weights.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        weights = F.softmax(weights, dim=-1)
+        weights = self.dropout(weights)
+
         ###
         out = weights @ v # (B, T, T) @ (B, T, C) -> (B, T, C)
         return out
+
+class MultiHeadAttention(nn.Module):
+    """ multiple heads of self-attention in parallel """
+
+    def __init__(self, n_head, n_embd):
+        super().__init__()
+        ## YOUR CODE HERE
+       # num_heads = n_head 
+        head_size = n_embd // n_head
+        ## list of num_heads modules of type Head
+        self.heads = nn.ModuleList([Head(n_embd, head_size) for _ in range(n_head)])
+        self.proj = nn.Linear(n_embd, n_embd)  # projection finale 
+        self.dropout = nn.Dropout(dropout)
+
+        
+        
+    def forward(self, x):
+        ## YOUR CODE HERE
+        ## apply each head in self.heads to x and concat the results 
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        out = self.dropout(out)
+
+        return out
+
+class FeedForward(nn.Module):
+    """ a simple MLP with RELU """
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class Block(nn.Module):
+    """ A single bloc of multi-head attention """
+
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        #head_size = n_embd // n_head
+        #self.sa = MultiHeadAttention(n_head, n_embd)
+        #self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+        self.sa = MultiHeadAttention(n_head, n_embd)
+        self.ffwd = FeedForward(n_embd)
+
+    def forward(self, x):
+        #x = self.sa(x)
+        #x = self.ffwd(x)
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
+
+
+
 
 
 class BigramLanguageModel(nn.Module):
@@ -100,7 +164,15 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = Head(n_embd)
+        # Using Sequential to stack 3 Blocks
+        self.blocks = nn.Sequential(
+            Block(n_embd, n_head=4),  # First block
+            Block(n_embd, n_head=4),  # Second block
+            Block(n_embd, n_head=4)   # Third block
+        )
+        # Language model head
+        #self.lm_head = nn.Linear(n_embd, vocab_size)
+        self.ln_f = nn.LayerNorm(n_embd)  # Final LayerNorm
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -110,7 +182,9 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B,T,C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
         x = tok_emb + pos_emb # (B,T,C)
-        x = self.sa_head(x) # (B,T,C)
+        x = self.blocks(x) # (B,T,C)
+        x = self.ln_f(x)
+      
         logits = self.lm_head(x) # (B,T,vocab_size)
 
         if targets is None:
